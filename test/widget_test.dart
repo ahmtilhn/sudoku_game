d570a854e2bc2056af880f75890be4cdecb5f307
ahmtilhn <119415449+ahmtilhn@users.noreply.github.com>
2026-07-23 23:44:52 +0200

    Use in-memory preferences in widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sudoku_game/app.dart';
 import 'package:sudoku_game/data/local_progress_store.dart';
 
 void main() {
   testWidgets('home screen exposes the main game modes', (tester) async {
-    SharedPreferences.setMockInitialValues(<String, Object>{});
-    final store = await LocalProgressStore.create();
+    final store = await LocalProgressStore.createInMemory();
 
     await tester.pumpWidget(SudokuApp(store: store));
     await tester.pumpAndSettle();
