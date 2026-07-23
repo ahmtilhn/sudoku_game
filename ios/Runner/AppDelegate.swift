739df8e2eacbfafbf159804f956971c6464b8f39
ahmtilhn <119415449+ahmtilhn@users.noreply.github.com>
2026-07-24 00:03:34 +0200

    Add the iOS localization bridge

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,6 +3,8 @@ import UIKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+  private var localizationChannel: FlutterMethodChannel?
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
@@ -12,5 +14,46 @@ import UIKit
 
   func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
     GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+
+    let registrar = engineBridge.pluginRegistry.registrar(
+      forPlugin: "SudokuLocalizationBridge"
+    )
+    let channel = FlutterMethodChannel(
+      name: "com.devovia.sudoku/localization",
+      binaryMessenger: registrar.messenger()
+    )
+    channel.setMethodCallHandler { call, result in
+      guard call.method == "getStrings" else {
+        result(FlutterMethodNotImplemented)
+        return
+      }
+      guard
+        let arguments = call.arguments as? [String: Any],
+        let keys = arguments["keys"] as? [String]
+      else {
+        result(FlutterError(
+          code: "invalid_arguments",
+          message: "Localization keys are missing.",
+          details: nil
+        ))
+        return
+      }
+
+      var values: [String: String] = [:]
+      for key in keys {
+        let localized = NSLocalizedString(
+          key,
+          tableName: nil,
+          bundle: .main,
+          value: key,
+          comment: ""
+        )
+        if localized != key {
+          values[key] = localized
+        }
+      }
+      result(values)
+    }
+    localizationChannel = channel
   }
 }
