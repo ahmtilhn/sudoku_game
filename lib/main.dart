6ee9f05f0eca18c49bf26399a9233fabb30afc6b
ahmtilhn <119415449+ahmtilhn@users.noreply.github.com>
2026-07-24 18:16:10 +0200

    Initialize reminder notifications on app startup

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:flutter/material.dart';
 import 'app.dart';
 import 'data/local_progress_store.dart';
 import 'localization/app_strings.dart';
+import 'services/reminder_notification_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final store = await LocalProgressStore.create();
   final strings = await AppStrings.load();
+  await ReminderNotificationService.instance.initialize();
   runApp(SudokuApp(store: store, strings: strings));
 }
