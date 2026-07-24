e2b2af2f69cc7aae6cd49505cc43345067c04c7e
ahmtilhn <119415449+ahmtilhn@users.noreply.github.com>
2026-07-24 18:51:16 +0200

    Pin stable Play Games v2 and iOS-compatible foundations

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 }
 
 dependencies {
-    implementation("com.google.android.gms:play-services-games-v2:21.0.0")
+    implementation("com.google.android.gms:play-services-games-v2:19.0.0")
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
