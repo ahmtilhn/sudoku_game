f5176ae99d4992ee7f35dc4dc1be423ab734d5b8
ahmtilhn <119415449+ahmtilhn@users.noreply.github.com>
2026-07-24 18:57:42 +0200

    Initialize Firebase challenge notifications

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'app.dart';
 import 'data/local_progress_store.dart';
 import 'localization/app_strings.dart';
 import 'services/ads_service.dart';
+import 'services/push_notification_service.dart';
 import 'services/reminder_notification_service.dart';
 
 Future<void> main() async {
@@ -13,6 +14,7 @@ Future<void> main() async {
   final store = await LocalProgressStore.create();
   final strings = await AppStrings.load();
   await ReminderNotificationService.instance.initialize();
+  await PushNotificationService.instance.initialize();
   runApp(SudokuApp(store: store, strings: strings));
   unawaited(AdsService.instance.initialize());
 }
