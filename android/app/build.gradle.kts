ff113e10138b7f21672235007bcaf0e381554853
ahmtilhn <119415449+ahmtilhn@users.noreply.github.com>
2026-07-28 00:21:20 +0200

    Raise Android minimum SDK for current Billing support

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 
     defaultConfig {
         applicationId = "com.devoviastudio.sudoku"
-        minSdk = maxOf(23, flutter.minSdkVersion)
+        minSdk = maxOf(24, flutter.minSdkVersion)
         targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
